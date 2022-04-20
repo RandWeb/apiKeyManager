d7e673b066da62e6bf6fa6cf7f4662b309cb304a
Mehrdad Tabesh <inf.websearchsite@gmail.com>
2022-04-20 09:30:02 -0700

    add edit to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+const idInput = document.querySelector('#id');
 const userNameInput = document.querySelector('#user');
 const keyInput = document.querySelector('#key');
 const secretInput = document.querySelector('#secret');
@@ -13,7 +14,7 @@ const apis = [];
 
 function EditApi(elment) {
     const childrenParent = elment.parentElement.parentElement.children;
-    let id = childrenParent[0];
+    idInput.value = childrenParent[0].textContent;
     userNameInput.value = childrenParent[1].textContent;
     keyInput.value = childrenParent[2].textContent;
     secretInput.value = childrenParent[3].textContent;
@@ -44,15 +45,12 @@ function ShowToast(messaage) {
     let open = document.querySelector(".notification__open");
     let toastMessage = document.querySelector(".notification__message");
     toastMessage.innerHTML = messaage;
-    open.classList.add('opening')
+    open.classList.add('opening');
     toastMessage.classList.add('message__opening');
     toast.classList.add('notification__opening');
     setTimeout(() => { toastMessage.className = toastMessage.className.replace("message__opening", ""); }, 5000);
     setTimeout(() => { open.className = open.className.replace("opening", ""); }, 6000);
-    setTimeout(() => {
-        toast.className = toast.className.replace("notification__opening", "");
-    }, 7000);
-
+    setTimeout(() => { toast.className = toast.className.replace("notification__opening", ""); }, 7000);
 }
 
 
@@ -64,19 +62,32 @@ function CreateRowTable() {
         apiKey: inputs[1].value,
         apiSecret: inputs[2].value
     };
-    let edit = apis.find(item => item.userName === newApi.userName)
-    if (edit) {
-        let idndexObj = apis.findIndex(item=>item.userName===newApi.userName);
-        apis[idndexObj] = newApi.id = edit.id;
-        apis.forEach(item=>{
+    let edit = apis.filter(item => item.id == idInput.value)
+    if (edit.length == 1) {
+        newApi.id = edit[0].id;
+        let idndexObj = apis.findIndex(item => item.id == newApi.id);
+        apis[idndexObj] = newApi;
+        let list = "";
+        let tbody = document.querySelector('tbody');
+        tbody.innerHTML = "";
+        apis.forEach(item => {
             let tr = document.createElement("tr");
-            tr.innerHTML = (`<td>${apis.length + 1}</td><td>${newApi['userName']}</td><td>${newApi['apiKey']}</td><td>${newApi['apiSecret']}</td><td><button class="btn delete">delete</button></td><td><button class="btn edit" onClick="EditApi(this)">edit</button></td>`);
-            document.querySelector('tbody').appendChild(tr);
-        })
-    }else{
-        apis.push(newApi.id = apis.length+1);
+            tr.innerHTML = (`<td>${item.id}</td><td>${item.userName}</td><td>${item.apiKey}</td><td>${item.apiSecret}</td><td><button class="btn delete">delete</button></td><td><button class="btn edit" onClick="EditApi(this)">edit</button></td>`);
+            console.log(tr);
+            tbody.appendChild(tr);
+        });
+
+    } else {
+        newApi.id = apis.length + 1;
+        apis.push(newApi);
         let tr = document.createElement("tr");
-        tr.innerHTML = (`<td>${apis.length + 1}</td><td>${newApi['userName']}</td><td>${newApi['apiKey']}</td><td>${newApi['apiSecret']}</td><td><button class="btn delete">delete</button></td><td><button class="btn edit" onClick="EditApi(this)">edit</button></td>`);
+        tr.innerHTML = (`<td>${apis.length}</td><td>${newApi['userName']}</td><td>${newApi['apiKey']}</td><td>${newApi['apiSecret']}</td><td><button class="btn delete">delete</button></td><td><button class="btn edit" onClick="EditApi(this)">edit</button></td>`);
         document.querySelector('tbody').appendChild(tr);
     }
+    emptyForm();
+}
+
+function emptyForm() {
+    inputs.forEach(item => item.value = "");
+    idInput.value = "";
 }
